4910484634b4a08754fe1783945f2c4a0be003e5
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-07-19 16:26:10 -0300

    restart

--- script.js
+++
@@ -1,8 +0,0 @@
-const boxClass = document.querySelectorAll('.box')
-const openBtn = document.querySelectorAll('#open')
-const closeBtn = document.querySelectorAll('#close')
-
-openBtn.addEventListener('clisck', () => {
-  box.classList.add('')
-})
-
